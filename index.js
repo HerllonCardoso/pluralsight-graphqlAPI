3fcc7ab1ce8f1469c5ba1291e93ceab426e79d7c
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-03-06 14:28:30 -0300

    nesting resolvers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server');
 const SessionAPI = require('./datasources/sessions');
+const SpeakerAPI = require('./datasources/speakers');
+
 
 const {typeDefs, resolvers} = require('./graphql')
 
 const dataSources = () => ({
   sessionAPI: new SessionAPI(),
+  speakerAPI: new SpeakerAPI(),
+
 });
 
 const server = new ApolloServer({
