d845063d40dac80fbf1b2588dad3e565ec7b85a6
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-03-09 09:08:11 -0300

    throwing errors

--- resolvers/session.js
+++ resolvers/session.js
@@ -1,11 +1,17 @@
+const { ApolloError } = require('apollo-server');
 const _ = require('lodash');
 
 module.exports = {
   speakers: async (session, args, { dataSources }, info) => {
-    const speakers = await dataSources.speakerAPI.getSpeakers();
+    try{
+      const speakers = await dataSources.speakerAPI.getSpeakers();
     const returns = speakers.filter((speaker) => {
       return _.filter(session.speakers, { id: speaker.id }).length > 0;
     });
     return returns;
+    } catch (error) {
+      return new ApolloError("Unable to get speakers", "SPEAKAPIERROR", {token: "UNIQUETOKEN"})
+    }
+    
   },
 };
